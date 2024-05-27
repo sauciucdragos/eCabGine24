b14f712040f29610de3c30ef87c253910bafd541
Beruica Narcisa <narcisa.beruica@gmail.com>
2024-05-27 12:22:55 +0300

    first commit-login and registration page

---
+++ app/Controllers/Users.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\UserModel;
+
+class Users extends BaseController
+{
+    public function index()
+    {
+        $data=[];
+        helper(['form']);
+
+        echo view('templates/header', $data);
+        echo view('login',$data);
+        echo view('templates/footer', $data);
+    }
+
+    public function register()
+    {
+        $data=[];
+        helper(['form']);
+
+        if($this->request->getMethod()=='post'){
+            $rules=[
+                'firstname'=>'required|min_length[3]|max_length[20]',
+                'lastname'=>'required|min_length[3]|max_length[30]',
+                'email'=>'required|min_length[6]|max_length[50]|valid_email|is_unique[users.email]',
+                'password'=>'required|min_length[8]|max_length[255]',
+                'password_confirm'=> 'matches[password]',
+            ];
+
+            if(!$this ->validate($rules)){
+                $data['validation']=$this->validator;
+            }else{
+                $model =new UserModel();
+
+                $newData = [
+                    'firstname'=>$this->request->getVar('firstname'),
+                    'lastname'=>$this->request->getVar('lastname'),
+                    'email'=>$this->request->getVar('email'),
+                    'password'=>$this->request->getVar('password'),
+                ];
+                $model->save($newData);
+                $session = session();
+                $session->setFlashdata('success', 'Successful Registration');
+                return redirect()->to('/');
+            }
+        }
+
+        echo view('templates/header', $data);
+        echo view('register',$data);
+        echo view('templates/footer', $data); 
+    }
+}
---
+++ app/Views/templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/">eCabGine Login</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/register">Register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    
